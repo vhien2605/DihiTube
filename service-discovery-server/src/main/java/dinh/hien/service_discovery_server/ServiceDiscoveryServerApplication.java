ab8e5eb0fc000ed3b1612e129ea6318f72b8e319
hienonichan <hvu6582@gmail.com>
2026-02-20 19:29:45 +0700

    init project, config service discovery with spring eureka

---
+++ service-discovery-server/src/main/java/dinh/hien/service_discovery_server/ServiceDiscoveryServerApplication.java
@@ -0,0 +1,16 @@
+package dinh.hien.service_discovery_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServiceDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDiscoveryServerApplication.class, args);
+	}
+
+}
---
+++ service-discovery-server/src/test/java/dinh/hien/service_discovery_server/ServiceDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dinh.hien.service_discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
